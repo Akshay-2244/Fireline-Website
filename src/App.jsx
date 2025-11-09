0a576be5e8e7197a14dcac72d44ba857bdc0dd2d
Akshay-2244 <akshaycris003@gmail.com>
2025-11-09 13:40:28 +0530

    added NewsSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import TopBanner from "./components/TopBanner";
 import Footer from "./components/Footer";
+import NewsSection from "./sections/NewsSection";
 
 function App() {
   return (
     <>
       <Header />
       <TopBanner />
+      <NewsSection />
       <Footer />
     </>
   );
